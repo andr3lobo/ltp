0ee9618e0a5cff65abfdf309dbb9fdd65bfd787a
andr3lobo <andre.assis.lobo@gmail.com>
2023-04-25 22:42:43 -0300

    Exercício 1

--- StartJava/src/lista1/Salario.java
+++ StartJava/src/lista1/Salario.java
@@ -3,11 +3,27 @@ package lista1;
 public class Salario {
 
 	public static void main(String[] args) {
-		// letra a
+		/*letra a)*/
+		
 		int salario = 10000;
 		System.out.println(salario);
 		
-		//letra b
+		/*letra b)*/
+		float salarioF = (float) salario;
+		System.out.println(salarioF);
+		
+		/*letra c)*/
+		float bonus1 = 200;
+		System.out.println("Salário acrescido do bônus 1:" + (salarioF + bonus1));
+		
+		/* letra d) */
+		float percentualBonus2 = 10;
+		float bonus2 = salarioF*percentualBonus2/100;
+		System.out.println("Salário acrescido do bônus 2: "+ (salarioF + bonus2));
+		
+		/* plus: imprimindo com duas casas decimais */
+		float s = salarioF+bonus2;
+		System.out.printf("Salário com bônus 2: R$ %.2f",s);
 
 	}
 
