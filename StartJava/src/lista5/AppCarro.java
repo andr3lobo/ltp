1639d6a3173b4bbd9b0883441ca5924643ad4bac
andrelobo <andrelobo@localhost>
2023-08-30 18:44:38 -0300

    Lista 5

---
+++ StartJava/src/lista5/AppCarro.java
@@ -0,0 +1,19 @@
+package lista5;
+
+public class AppCarro {
+
+	public static void main(String[] args) {
+		Carro c1 = new Carro();
+		c1.setVelocidadeAtual(50);
+		
+		Carro c2 = new Carro("Chevrolet", "SPIN LTZ", "Prata");
+		c2.setVelocidadeAtual(40);
+		
+		double mediaVelocidade = (c1.getVelocidadeAtual() + c2.getVelocidadeAtual())/2;
+		System.out.println("A media de velocidade é: "+ mediaVelocidade);
+		
+		
+		
+	}
+
+}
---
+++ StartJava/src/lista5/Carro.java
@@ -0,0 +1,110 @@
+package lista5;
+
+
+public class Carro {
+	private String marca;
+	private String modelo;
+	private String cor;
+	private String placa;
+	private double motor;
+	private boolean ligado;
+	private int capacidadeTanque;
+	private boolean pilotoAutomatico;
+	private double velocidadeMax;
+	private double velocidadeAtual;
+	
+	//método construtor sem parâmetro
+	public Carro () {
+	}
+	
+	//método construtor com parâmetro
+	public Carro (String ma, String mo, String c) {
+		this.marca = ma;
+		this.modelo = mo;
+		this.cor = c;
+	}
+
+	
+	public double getVelocidadeAtual() {
+		return velocidadeAtual;
+	}
+
+	public void setVelocidadeAtual(double velocidadeAtual) {
+		this.velocidadeAtual = velocidadeAtual;
+	}
+
+	public String getMarca() {
+		return marca;
+	}
+
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
+
+	public String getModelo() {
+		return modelo;
+	}
+
+	public void setModelo(String modelo) {
+		this.modelo = modelo;
+	}
+
+	public String getCor() {
+		return cor;
+	}
+
+	public void setCor(String cor) {
+		this.cor = cor;
+	}
+
+	public String getPlaca() {
+		return placa;
+	}
+
+	public void setPlaca(String placa) {
+		this.placa = placa;
+	}
+
+	public double getMotor() {
+		return motor;
+	}
+
+	public void setMotor(double motor) {
+		this.motor = motor;
+	}
+
+	public boolean isLigado() {
+		return ligado;
+	}
+
+	public void setLigado(boolean ligado) {
+		this.ligado = ligado;
+	}
+
+	public int getCapacidadeTanque() {
+		return capacidadeTanque;
+	}
+
+	public void setCapacidadeTanque(int capacidadeTanque) {
+		this.capacidadeTanque = capacidadeTanque;
+	}
+
+	public boolean isPilotoAutomatico() {
+		return pilotoAutomatico;
+	}
+
+	public void setPilotoAutomatico(boolean pilotoAutomatico) {
+		this.pilotoAutomatico = pilotoAutomatico;
+	}
+
+	public double getVelocidadeMax() {
+		return velocidadeMax;
+	}
+
+	public void setVelocidadeMax(double velocidadeMax) {
+		this.velocidadeMax = velocidadeMax;
+	}
+	
+	
+	
+}
